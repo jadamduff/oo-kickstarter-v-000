01f691390eecd90393dfa8325bee5a49f108fdd5
Adam Duff <j.adam.duff@gmail.com>
2018-08-17 19:01:24 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,7 +3,14 @@ class Backer
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
-  
+  def backed_projects
+    @backed_projects
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+  end
 end
